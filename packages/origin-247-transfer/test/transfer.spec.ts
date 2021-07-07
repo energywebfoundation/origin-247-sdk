aaea748cc3f07f49febfc670928ceabcc08c3af1
Mateusz Koteja <mateusz.koteja@leocode.com>
2021-07-07 14:40:00 +0200

    fix(origin-247-certificate): save certificate to database after persisting event

--- packages/origin-247-transfer/test/transfer.spec.ts
+++ packages/origin-247-transfer/test/transfer.spec.ts
@@ -1,5 +1,5 @@
 import { Test } from '@nestjs/testing';
-import { QueryHandler, QueryBus, EventBus } from '@nestjs/cqrs';
+import { QueryHandler, QueryBus, EventBus, CommandBus } from '@nestjs/cqrs';
 import {
     GenerationReadingStoredEvent,
     GetTransferSitesQuery,
@@ -9,23 +9,9 @@ import {
 
 describe('Transfer module', () => {
     it('should build', async () => {
-        const getSitesQuery = jest.fn(() => ({ buyerId: 'buyer', sellerId: 'seller' }));
-
-        @QueryHandler(GetTransferSitesQuery)
-        class SitesQueryHandler implements IGetTransferSitesQueryHandler {
-            async execute(query: GetTransferSitesQuery) {
-                return getSitesQuery();
-            }
-        }
-
-        const moduleFixture = await Test.createTestingModule({
-            imports: [TransferModuleForUnitTest.register({ validateCommands: [] })],
-            providers: [SitesQueryHandler]
-        }).compile();
-
-        const app = moduleFixture.createNestApplication();
-        const queryBus = await app.resolve<QueryBus>(QueryBus);
-        const eventBus = await app.resolve<EventBus>(EventBus);
+        const { app, queryBus, commandBus, eventBus } = await setup({
+            sites: { buyerId: 'buyer1', sellerId: 'seller1' }
+        });
 
         await app.init();
 
@@ -43,5 +29,35 @@ describe('Transfer module', () => {
         await new Promise((resolve) => setTimeout(resolve, 3000));
 
         expect(getSitesQuery).toBeCalledTimes(1);
+
+        await app.close();
     });
 });
+
+const setup = async (options: { sites: { buyerId: string; sellerId: string } }) => {
+    const getSitesQuery = jest.fn(() => options.sites);
+
+    @QueryHandler(GetTransferSitesQuery)
+    class SitesQueryHandler implements IGetTransferSitesQueryHandler {
+        async execute(query: GetTransferSitesQuery) {
+            return getSitesQuery();
+        }
+    }
+
+    const moduleFixture = await Test.createTestingModule({
+        imports: [TransferModuleForUnitTest.register({ validateCommands: [] })],
+        providers: [SitesQueryHandler]
+    }).compile();
+
+    const app = moduleFixture.createNestApplication();
+    const queryBus = await app.resolve<QueryBus>(QueryBus);
+    const commandBus = await app.resolve<CommandBus>(CommandBus);
+    const eventBus = await app.resolve<EventBus>(EventBus);
+
+    return {
+        app,
+        queryBus,
+        commandBus,
+        eventBus
+    };
+};
