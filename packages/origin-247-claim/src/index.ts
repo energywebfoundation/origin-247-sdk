536951b6328b93a9fce2ee5057c621b1c1fabe04
patryk-kochanski <patryk.kochanski@gmail.com>
2021-08-13 13:36:32 +0200

    chore(origin-247-claim): Add exports to index

--- packages/origin-247-claim/src/index.ts
+++ packages/origin-247-claim/src/index.ts
@@ -1,1 +1,38 @@
 export * from './algorithms';
+export * from './interfaces';
+
+export {
+    tableName as matchResultTableName,
+    MatchResultEntity
+} from './repositories/MatchResult/MatchResult.entity';
+export * from './repositories/MatchResult/MatchResult.repository';
+export * from './repositories/MatchResult/MatchResultPostgres.repository';
+
+export {
+    tableName as leftoverConsumptionTableName,
+    LeftoverConsumptionEntity
+} from './repositories/LeftoverConsumption/LeftoverConsumption.entity';
+export {
+    FindOptions as LeftoverConsumptionFindOptions,
+    LeftoverConsumptionColumns,
+    NewLeftoverConsumption,
+    LeftoverConsumptionRepository,
+    LEFTOVER_CONSUMPTION_REPOSITORY
+} from './repositories/LeftoverConsumption/LeftoverConsumption.repository';
+export * from './repositories/LeftoverConsumption/LeftoverConsumptionPostgres.repository';
+
+export {
+    tableName as excessGenerationTableName,
+    ExcessGenerationEntity
+} from './repositories/ExcessGeneration/ExcessGeneration.entity';
+export {
+    FindOptions as ExcessGenerationFindOptions,
+    ExcessGenerationColumns,
+    NewExcessGeneration,
+    ExcessGenerationRepository,
+    EXCESS_GENERATION_REPOSITORY
+} from './repositories/ExcessGeneration/ExcessGeneration.repository';
+export * from './repositories/ExcessGeneration/ExcessGenerationPostgress.repository';
+
+export * from './claim.module';
+export * from './claim.service';
