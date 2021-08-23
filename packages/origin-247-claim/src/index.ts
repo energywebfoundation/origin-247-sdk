96c4caa8d1666d17837abb88390e6c72db850891
Mateusz Koteja <mateusz.koteja@leocode.com>
2021-08-23 17:02:32 +0200

    chore(origin-247-claim): cleanup exports and export ExcessGenerationEntity

--- packages/origin-247-claim/src/index.ts
+++ packages/origin-247-claim/src/index.ts
@@ -1,25 +1,11 @@
+import { MatchResultEntity } from './repositories/MatchResult/MatchResult.entity';
+import { LeftoverConsumptionEntity } from './repositories/LeftoverConsumption/LeftoverConsumption.entity';
+import { ExcessGenerationEntity } from './repositories/ExcessGeneration/ExcessGeneration.entity';
+
 export * from './algorithms';
 export * from './interfaces';
 
-export {
-    tableName as matchResultTableName,
-    MatchResultEntity
-} from './repositories/MatchResult/MatchResult.entity';
-export * from './repositories/MatchResult/MatchResult.repository';
-export * from './repositories/MatchResult/MatchResultPostgres.repository';
-
-export {
-    tableName as leftoverConsumptionTableName,
-    LeftoverConsumptionEntity
-} from './repositories/LeftoverConsumption/LeftoverConsumption.entity';
-export {
-    FindOptions as LeftoverConsumptionFindOptions,
-    LeftoverConsumptionColumns,
-    NewLeftoverConsumption,
-    LeftoverConsumptionRepository,
-    LEFTOVER_CONSUMPTION_REPOSITORY
-} from './repositories/LeftoverConsumption/LeftoverConsumption.repository';
-export * from './repositories/LeftoverConsumption/LeftoverConsumptionPostgres.repository';
+export const entities = [MatchResultEntity, LeftoverConsumptionEntity, ExcessGenerationEntity];
 
 export * from './claim.module';
 export * from './claim.facade';
