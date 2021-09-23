edafdee92e3c74d0cd380e2b379774df2a8ccffa
Joseph Bagaric <bagaricjos@gmail.com>
2021-09-23 11:02:43 +0200

    chore(notary): remove unused hardhat task

--- packages/notary/hardhat.config.ts
+++ packages/notary/hardhat.config.ts
@@ -1,18 +1,10 @@
-import { task, HardhatUserConfig } from 'hardhat/config';
+import { HardhatUserConfig } from 'hardhat/config';
 import '@nomiclabs/hardhat-waffle';
 import '@typechain/hardhat';
 import '@nomiclabs/hardhat-ethers';
 
 // This adds support for typescript paths mappings
 import 'tsconfig-paths/register';
-
-task('accounts', 'Prints the list of accounts', async (args, hre) => {
-    const accounts = await hre.ethers.getSigners();
-
-    for (const account of accounts) {
-        console.log(await account.address);
-    }
-});
 
 const config: HardhatUserConfig = {
     solidity: '0.8.4',
