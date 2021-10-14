bc99e132c28adb7e9065895f7f92d9787b7c705f
Mateusz Koteja <mateusz.koteja@leocode.com>
2021-10-14 16:02:55 +0200

    fix(origin-247-transfer): use better batching system

--- packages/origin-247-transfer/src/batch/transfer.batch.ts
+++ packages/origin-247-transfer/src/batch/transfer.batch.ts
@@ -8,14 +8,28 @@ export class AwaitingTransferEvent implements IEvent {}
 
 @EventsHandler(AwaitingTransferEvent)
 export class AwaitingTransferEventHandler implements IEventHandler<AwaitingTransferEvent> {
+    public handle: VoidFunction;
+    private unsubscribe: VoidFunction;
+
     constructor(
         @Inject(BATCH_CONFIGURATION_TOKEN)
         private batchConfiguration: BatchConfiguration,
         private transferService: TransferService
-    ) {}
+    ) {
+        const { trigger, unsubscribe } = queueThrottle(
+            () => this.transferService.transferTask(),
+            this.batchConfiguration.transferAggregateSeconds
+        );
 
-    public handle = queueThrottle<AwaitingTransferEvent>(
-        () => this.transferService.transferTask(),
-        this.batchConfiguration.transferAggregateSeconds
-    );
+        this.handle = trigger;
+        this.unsubscribe = unsubscribe;
+    }
+
+    public onApplicationBootstrap() {
+        this.handle();
+    }
+
+    public onModuleDestroy() {
+        this.unsubscribe();
+    }
 }
--- packages/origin-247-transfer/src/batch/validate.batch.ts
+++ packages/origin-247-transfer/src/batch/validate.batch.ts
@@ -8,14 +8,28 @@ export class AwaitingValidationEvent implements IEvent {}
 
 @EventsHandler(AwaitingValidationEvent)
 export class AwaitingValidationEventHandler implements IEventHandler<AwaitingValidationEvent> {
+    public handle: VoidFunction;
+    private unsubscribe: VoidFunction;
+
     constructor(
         @Inject(BATCH_CONFIGURATION_TOKEN)
         private batchConfiguration: BatchConfiguration,
         private validateService: ValidateService
-    ) {}
+    ) {
+        const { trigger, unsubscribe } = queueThrottle(
+            () => this.validateService.validateTask(),
+            this.batchConfiguration.validateAggregateSeconds
+        );
 
-    public handle = queueThrottle<AwaitingValidationEvent>(
-        () => this.validateService.validateTask(),
-        this.batchConfiguration.validateAggregateSeconds
-    );
+        this.handle = trigger;
+        this.unsubscribe = unsubscribe;
+    }
+
+    public onApplicationBootstrap() {
+        this.handle();
+    }
+
+    public onModuleDestroy() {
+        this.unsubscribe();
+    }
 }
