139206a1a15b9a92d0b0cf9a8ea1ac815216807a
patryk-kochanski <patryk.kochanski@gmail.com>
2021-11-09 15:32:20 +0100

    chore(origin-247-certificate): remove unused imports

--- packages/origin-247-certificate/src/evented-certificate/repositories/CertificateCommand/CertificateCommandPostgres.repository.ts
+++ packages/origin-247-certificate/src/evented-certificate/repositories/CertificateCommand/CertificateCommandPostgres.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, Between, In } from 'typeorm';
+import { Repository } from 'typeorm';
 
 import { CertificateCommandEntity } from './CertificateCommand.entity';
 import {
